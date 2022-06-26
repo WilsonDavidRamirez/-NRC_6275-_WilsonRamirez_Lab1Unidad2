eb2179d56c4583b14284d50fcf27fbd86f4d1bc2
Wilson R <wdramirez@espe.edu.ec>
2022-06-26 14:39:56 -0500

    Tercero

--- templates/statics/js/logica.js
+++ templates/statics/js/logica.js
@@ -3,6 +3,7 @@ var canvasHeight = 400;
 
 function starGame(){
     gameCanvas.start();
+    player=new createPlayer(30,30,10);
 }
 
 var gameCanvas={
@@ -13,4 +14,19 @@ var gameCanvas={
         this.context=this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas,document.body.childNodes[0]);
     }
+}
+
+var player;
+
+var playerYposition = 200;
+
+function createPlayer(width, height, x){
+    this.width=width;
+    this.height=height;
+    this.x=x;
+    this.y=playerYposition;
+
+    ctx=gameCanvas.context;
+    ctx.fillStyle="blue";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
 }
